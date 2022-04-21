e0f687df636323105336e690cf208778599279d1
Nico Thomas <thomasnico@web.de>
2022-04-21 18:55:28 +0200

    Some refactoring

--- ToShop_Plugin_UI/src/main/java/com/toshop/plugins/ui/mvc/views/ShoppingListMenuView.java
+++ ToShop_Plugin_UI/src/main/java/com/toshop/plugins/ui/mvc/views/ShoppingListMenuView.java
@@ -1,5 +1,6 @@
 package com.toshop.plugins.ui.mvc.views;
 
+import com.toshop.domain.entities.Product;
 import com.toshop.domain.entities.ShoppingList;
 import com.toshop.domain.entities.ShoppingListItem;
 import com.toshop.plugins.ui.mvc.base.View;
@@ -15,8 +16,8 @@ import java.awt.event.FocusListener;
 
 public class ShoppingListMenuView extends View<ShoppingListMenuController> {
 
-    private JList<Object> shoppingListItemList;
-    private JList<Object> suggestedList;
+    private JList<ShoppingListItem> shoppingListItemList;
+    private JList<Product> suggestedList;
 
     public ShoppingListMenuView(ShoppingList shoppingList) {
         super();
@@ -44,13 +45,86 @@ public class ShoppingListMenuView extends View<ShoppingListMenuController> {
 
         // Split main panel into two parts
         var splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, main, new JPanel());
-        splitPane.setDividerLocation(0.75);
-        splitPane.setResizeWeight(0.75);
+        splitPane.setDividerLocation(0.85);
+        splitPane.setResizeWeight(1);
 
-        // List of all items in the ShoppingList using a JList
+        // Shopping List on the left
+        JPanel shoppingListPanel = buildShoppingListPanel();
+        splitPane.setLeftComponent(shoppingListPanel);
+
+        // Add Product on the right
+        JPanel addProductPanel = buildAddProductPanel();
+        splitPane.setRightComponent(addProductPanel);
+
+        main.add(splitPane, BorderLayout.CENTER);
+
+        return main;
+    }
+
+    private JPanel buildAddProductPanel() {
+        var inputPanel = new JPanel();
+        inputPanel.setLayout(new BorderLayout());
+        var topInputPanel = new JPanel();
+        topInputPanel.setLayout(new BorderLayout());
+        var productNameInputField = new JTextField();
+        productNameInputField.setFont(ui.getDefaultFont().deriveFont(16f));
+        productNameInputField.setText("Enter product name...");
+        productNameInputField.setForeground(Color.LIGHT_GRAY);
+        productNameInputField.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (productNameInputField.getText().equals("Enter product name...")) {
+                    productNameInputField.setText("");
+                    productNameInputField.setForeground(Color.BLACK);
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (productNameInputField.getText().equals("")) {
+                    productNameInputField.setText("Enter product name...");
+                    productNameInputField.setForeground(Color.LIGHT_GRAY);
+                }
+            }
+        });
+        topInputPanel.add(productNameInputField, BorderLayout.CENTER);
+        // Add JSpinner next to the input field
+        var productAmountSpinner = new JSpinner(new SpinnerNumberModel(1, 1, null, 1));
+        // Set spinner width to fit 2 digits
+        productAmountSpinner.setPreferredSize(new Dimension(50, 20));
+        productAmountSpinner.setFont(ui.getDefaultFont().deriveFont(16f));
+        topInputPanel.add(productAmountSpinner, BorderLayout.EAST);
+        inputPanel.add(topInputPanel, BorderLayout.NORTH);
+        suggestedList = new JList<>(controller.getSuggestedProducts().toArray(new Product[0]));
+        suggestedList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        suggestedList.setLayoutOrientation(JList.VERTICAL);
+        suggestedList.setVisibleRowCount(-1);
+        suggestedList.setFont(ui.getDefaultFont().deriveFont(16f));
+        // Add double click action to list that adds the selected element to the shopping list
+        suggestedList.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                if (evt.getClickCount() >= 2) {
+                    controller.addItem(suggestedList.getSelectedValue().toString(), (int) productAmountSpinner.getValue());
+                    evt.consume();
+                }
+            }
+        });
+        inputPanel.add(suggestedList, BorderLayout.CENTER);
+        // Add button that adds the input field to the shopping list
+        var addButton = new JButton("Add");
+        addButton.addActionListener(e -> {
+            controller.addItem(productNameInputField.getText(), (int) productAmountSpinner.getValue());
+            productNameInputField.setText("");
+        });
+        inputPanel.add(addButton, BorderLayout.SOUTH);
+        return inputPanel;
+    }
+
+    private JPanel buildShoppingListPanel() {
         var listPanel = new JPanel();
         listPanel.setLayout(new BorderLayout());
-        shoppingListItemList = new JList<>(controller.currentShoppingList.getItems().toArray());
+        shoppingListItemList = new JList<>(controller.currentShoppingList.getItems().toArray(new ShoppingListItem[0]));
         shoppingListItemList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         shoppingListItemList.setLayoutOrientation(JList.VERTICAL);
         shoppingListItemList.setVisibleRowCount(-1);
@@ -87,12 +161,12 @@ public class ShoppingListMenuView extends View<ShoppingListMenuController> {
         // Allow reordering of items by dragging them
         shoppingListItemList.setTransferHandler(new TransferHandler() {
             @Override
-            public boolean canImport(TransferHandler.TransferSupport info) {
+            public boolean canImport(TransferSupport info) {
                 return info.isDataFlavorSupported(DataFlavor.stringFlavor);
             }
 
             @Override
-            public boolean importData(TransferHandler.TransferSupport info) {
+            public boolean importData(TransferSupport info) {
                 if (!info.isDrop()) {
                     return false;
                 }
@@ -127,78 +201,12 @@ public class ShoppingListMenuView extends View<ShoppingListMenuController> {
         shoppingListItemList.setDragEnabled(true);
 
         listPanel.add(shoppingListItemList, BorderLayout.CENTER);
-
-        // Add list to left side of split pane
-        splitPane.setLeftComponent(listPanel);
-
-        // Panel that contains a input field for entering new products at the top and below it a JList with suggested products
-        var inputPanel = new JPanel();
-        inputPanel.setLayout(new BorderLayout());
-        var topInputPanel = new JPanel();
-        topInputPanel.setLayout(new BorderLayout());
-        var productNameInputField = new JTextField();
-        productNameInputField.setFont(ui.getDefaultFont().deriveFont(16f));
-        productNameInputField.setText("Enter product name...");
-        productNameInputField.setForeground(Color.LIGHT_GRAY);
-        productNameInputField.addFocusListener(new FocusListener() {
-            @Override
-            public void focusGained(FocusEvent e) {
-                if (productNameInputField.getText().equals("Enter product name...")) {
-                    productNameInputField.setText("");
-                    productNameInputField.setForeground(Color.BLACK);
-                }
-            }
-
-            @Override
-            public void focusLost(FocusEvent e) {
-                if (productNameInputField.getText().equals("")) {
-                    productNameInputField.setText("Enter product name...");
-                    productNameInputField.setForeground(Color.LIGHT_GRAY);
-                }
-            }
-        });
-        topInputPanel.add(productNameInputField, BorderLayout.CENTER);
-        // Add JSpinner next to the input field
-        var productAmountSpinner = new JSpinner(new SpinnerNumberModel(1, 1, null, 1));
-        // Set spinner width to fit 2 digits
-        productAmountSpinner.setPreferredSize(new Dimension(50, 20));
-        productAmountSpinner.setFont(ui.getDefaultFont().deriveFont(16f));
-        topInputPanel.add(productAmountSpinner, BorderLayout.EAST);
-        inputPanel.add(topInputPanel, BorderLayout.NORTH);
-        suggestedList = new JList<>(controller.getSuggestedProducts().toArray());
-        suggestedList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        suggestedList.setLayoutOrientation(JList.VERTICAL);
-        suggestedList.setVisibleRowCount(-1);
-        suggestedList.setFont(ui.getDefaultFont().deriveFont(16f));
-        // Add double click action to list that adds the selected element to the shopping list
-        suggestedList.addMouseListener(new java.awt.event.MouseAdapter() {
-            @Override
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                if (evt.getClickCount() >= 2) {
-                    controller.addItem(suggestedList.getSelectedValue().toString(), (int) productAmountSpinner.getValue());
-                    evt.consume();
-                }
-            }
-        });
-        inputPanel.add(suggestedList, BorderLayout.CENTER);
-        // Add button that adds the input field to the shopping list
-        var addButton = new JButton("Add");
-        addButton.addActionListener(e -> {
-            controller.addItem(productNameInputField.getText(), (int) productAmountSpinner.getValue());
-            productNameInputField.setText("");
-        });
-        inputPanel.add(addButton, BorderLayout.SOUTH);
-        splitPane.setRightComponent(inputPanel);
-
-        // Add splitpane to main panel
-        main.add(splitPane, BorderLayout.CENTER);
-
-        return main;
+        return listPanel;
     }
 
     public void update() {
-        shoppingListItemList.setListData(controller.currentShoppingList.getItems().toArray());
-        suggestedList.setListData(controller.getSuggestedProducts().toArray());
+        shoppingListItemList.setListData(controller.currentShoppingList.getItems().toArray(new ShoppingListItem[0]));
+        suggestedList.setListData(controller.getSuggestedProducts().toArray(new Product[0]));
     }
 
 }
